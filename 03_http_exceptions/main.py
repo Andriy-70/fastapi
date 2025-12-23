2015dcf7c580e818ebc2bb690a9fff2bc4c9ab65
Andriy-70 <andrijptrvc@gmail.com>
2025-12-23 14:31:59 +0200

    feat: add http exceptions

--- 03_http_exceptions/main.py
+++ 03_http_exceptions/main.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, Field, EmailStr
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 class User(BaseModel):
     username: str = Field(min_length=4, max_length=30)
@@ -17,18 +17,19 @@ class Task(BaseModel):
     author: User
     tags: list[Tag] = []
 
-    def __str__(self):
-        return (f"title: {self.title}\n"
-                f"desc: {self.description}\n"
-                f"priority: {self.priority}\n"
-                f"is_finished: {self.is_finished}")
+task_db = []
 
+app = FastAPI()
 
-app = FastAPI(
-    title="Learn FastaApi"
-)
+@app.post("/tasks/", status_code=201)
+async def create_tasks(task: Task):
+    task_db.append(task)
+    return task
 
-@app.post("/task/create")
-async def create_task(task: Task):
-    print(task)
-    return {"message": "Task created", "data": task}
+@app.get("/tasks/{task_id}")
+async def get_task(task_id: int):
+
+    if task_id  len(task_db):
+        raise HTTPException(status_code=404, detail="нема такого таску")
+
+    return task_db[task_id]
